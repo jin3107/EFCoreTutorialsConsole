f0c581a35558bff6551d0267c6d4d2068ad31141
Huỳnh Tấn Chương <tanchuonghuynh3@gmail.com>
2024-09-20 18:53:50 +0700

    update

---
+++ EFCoreTutorialsConsole/Models/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorialsConsole.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        private const string connectionString = "Data Source=Rainy;Initial Catalog=EFCoreTutorialsConsole;Integrated Security=True;Trust Server Certificate=True";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
--- EFCoreTutorialsConsole/Program.cs
+++ EFCoreTutorialsConsole/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using EFCoreTutorialsConsole.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace EFCoreTutorialsConsole
+{
+    class Program
+    {
+        static void CreateDatabase()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            using var dbContext = new ProductDbContext();
+            string dbName = dbContext.Database.GetDbConnection().Database;
+
+            var results = dbContext.Database.EnsureCreated();
+            if (results) { Console.WriteLine($"Tạo Db {dbName} thành công!"); }
+            else { Console.WriteLine($"Không tạo được {dbName}");  }
+        }
+
+        static void DropDatabase()
+        {
+            Console.OutputEncoding= Encoding.UTF8;
+            using var dbContext = new ProductDbContext();
+            string dbName = dbContext.Database.GetDbConnection().Database;
+
+            var results = dbContext.Database.EnsureDeleted();
+            if (results) { Console.WriteLine($"Xóa Db {dbName} thành công!"); }
+            else { Console.WriteLine($"Không xóa được {dbName}"); }
+        }
+
+        static void Main(string[] args)
+        {
+            //CreateDatabase();
+            //DropDatabase();
+
+
+        }
+    }
+}
